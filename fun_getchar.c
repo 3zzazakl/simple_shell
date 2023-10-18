7b8acd984d7803be6769aaead147a3954aa9ea37
wessam18 <wessamabdelnabi903@gmail.com>
2023-10-18 10:26:05 +0300

    "getchar function"

---
+++ fun_getchar.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+/**
+ * _getchar - function to detect the end of file
+ *
+ * Return: as need
+*/
+int _getchar(void)
+{
+	char i;
+	ssize_t z = read(STDERR_FILENO, &i, 1);
+
+	if (z == 1)
+	{
+		return (i);
+	}
+	else if (z == 0)
+	{
+		return (EOF);
+	}
+	else
+	{
+		return (-1);
+	}
+}
