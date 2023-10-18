c97f8690f5b78c8fdec820b71723a135766ce6d7
3zzazakl <vanzekovichgr@gmail.com>
2023-10-18 09:42:07 +0300

    adding shell.c file

---
+++ shell.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * main - Main function for out simple shell project
+ * @argc: no of input arguments.
+ * @argv: Input array.
+ * Return: (Success) always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		file_mode(argv[1]);
+	}
+	else if (isatty(STDERR_FILENO) == 1)
+	{
+		interactive_mode();
+	}
+	else
+	{
+		non_interactive_mode();
+	}
+	return (0);
+}
