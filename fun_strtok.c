27cc6cddd76b856efeeb5d785b167d722756e085
wessam18 <wessamabdelnabi903@gmail.com>
2023-10-18 12:12:31 +0300

    "strtok function"

---
+++ fun_strtok.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+/**
+ * my_strtok - function to impliment strtok function
+ *
+ * @line: input to split
+ * @delim: the place split at it
+ * Return: as need
+*/
+char *my_strtok(char *line, const char *delim)
+{
+	char *s = NULL;
+	char *t;
+
+	if (line == NULL && s == NULL)
+		return (NULL);
+
+	if (line != NULL)
+		s = line;
+
+	while (*s != '\0' && _strchr(delim, *s) != NULL)
+		s++;
+
+	if (*s == '\0')
+	{
+		s = NULL;
+		return (NULL);
+	}
+	t = s;
+	while (*s != '\0' && strchr(delim, *s) == NULL)
+		s++;
+
+	if (*s != '\0')
+	{
+		*s = '\0';
+		s++;
+	}
+	return (t);
+}
