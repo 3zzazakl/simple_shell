45070b0e83b05c830cb3d7b6040fd5ac0f36de1d
3zzazakl <vanzekovichgr@gmail.com>
2023-10-18 10:02:51 +0300

    adding non-interactive file

--- shell.h
+++ shell.h
@@ -1,12 +1,17 @@
 #ifndef __SIMPLE_SHELL__
 #define __SIMPLE_SHELL__
 
-/**********HEADERS_DECLARATIONS****/
+/**********HEADERS_DECLARATIONS******************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
+
+/************FUNCTION_DECLARATIONS******************/
+void interactive_mode(void);
+void non_interactive_mode(void);
 
 #endif /*__SIMPLE_SHELL__*/
