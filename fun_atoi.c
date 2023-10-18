c83de143de8b12813efb8eb81870cffccaed18ea
wessam18 <wessamabdelnabi903@gmail.com>
2023-10-18 10:18:33 +0300

    "atoi function"

---
+++ fun_atoi.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+/**
+ * _atoi - function to convert str to int
+ *
+ * @str: str to be converted
+ * Return: as need
+ *
+*/
+int _atoi(const char *str)
+{
+	int res = 0;
+	int x = 1;
+
+	if (*str == '-')
+	{
+		x = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	return (res * x);
+}
--- fun_getline.c
+++ fun_getline.c
@@ -1,46 +1,50 @@
 #include "shell.h"
 /**
- * 
- * 
+ *_getline - function to read a line
+ *@line: line to read
+ *@buffer_size: max. total size of buffer
+ *@fd: include the result of the function
+ *Return: as need
+ *
 */
 ssize_t _getline(char **line, size_t *buffer_size, int fd)
 {
-    size_t rch = 0;
-    int cuch;
-    *line = (char *)malloc(BUFFER_SIZE);
+	size_t rch = 0;
+	int cuch;
+	*line = (char *)malloc(BUFFER_SIZE);
 
-    if (*line == NULL)
-    {
-        perror("malloc");
-        return (-1);
-    }
-    while(1)
-    {
-        cuch = read(fd, *line + rch, 1);
-        if (cuch < 0)
-        {
-            perror("read");
-            return (-1);
-        }
-        else if (cuch == 0 || (*line)[rch] == '\n')
-        {
-            if (rch == 0 && cuch == 0)
-            {
-                free(*line);
-                return (-1);
-            }
-            (*line)[rch] = '\0';
-            return (rch);
-        }
-        else
-        {
-            rch++;
-            if (rch >= *buffer_size)
-            {
-                *buffer_size += buffer_size;
-                *line = (char *)realloc(*line, *buffer_size);
+	if (*line == NULL)
+	{
+		perror("malloc");
+		return (-1);
+	}
+	while (1)
+	{
+		cuch = read(fd, *line + rch, 1);
+		if (cuch < 0)
+		{
+			perror("read");
+			return (-1);
+		}
+		else if (cuch == 0 || (*line)[rch] == '\n')
+		{
+			if (rch == 0 && cuch == 0)
+			{
+				free(*line);
+				return (-1);
+			}
+			(*line)[rch] = '\0';
+			return (rch);
+		}
+		else
+		{
+			rch++;
+			if (rch >= *buffer_size)
+			{
+				*buffer_size += buffer_size;
+				*line = (char *)realloc(*line, *buffer_size);
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
